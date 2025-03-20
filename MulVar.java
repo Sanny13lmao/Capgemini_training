3e5897239c903c525a2a4550c21e3d2807f80c99
Sanny13lmao <sanghamitragoswami13@gmail.com>
2025-03-20 21:22:13 +0530

    multiple vars

---
+++ MulVar.java
@@ -0,0 +1,11 @@
+package varAndDatatype;
+
+public class MulVar {
+	public static void main(String[] args){
+		int a,b,c;
+		a=b=c = 10; //this is how we can declare mul vars
+		System.out.println(c);
+		System.out.println(b);
+	}
+
+}
