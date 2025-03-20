507ed69d2cde19fc8bc39aa33f0c59da6127f79e
Sangha13 <sanghamitragoswami13@gmail.com>
2025-03-20 21:09:50 +0530

    concat

---
+++ concatenation.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class concatenation {
+	public static void main(String[] args){
+//		System.out.println(true + 10 + "hello"); //throws error
+		System.out.println("hello" + true + 10.05);
+		System.out.println(10+"hello"+true); //integers get joined with strings
+//		System.out.println(10+10.75+'A'+true+"hi"+"demo"+15); // throws error
+	}
+
+}
